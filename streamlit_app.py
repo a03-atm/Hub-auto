36439f2b6e62312f2df1b5e0006f1a309ce37f47
a03-atm <amrcarexpert@gmail.com>
2026-03-27 16:36:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Hub Auto", page_icon="🚗", layout="wide")
 
-with open("index.html", "r", encoding="utf-8") as f:
+with open("Hubauto.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
 st.components.v1.html(html_content, height=1000, scrolling=True)
